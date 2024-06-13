372640e591c7e63c14a5a0ac6e726172b6fd6454
groovyplanet <sneakersgroovy@gmail.com>
2024-06-13 10:40:52 +0900

    Oracle 함수 commit

--- 04함수_ETC문제.sql
+++ 04함수_ETC문제.sql
@@ -33,12 +33,15 @@ SELECT * FROM EMPLOYEES;
 
 --조건 1) 이름 컬럼은 first_name, last_name을 붙여서 출력합니다.
 --조건 2) 입사일자 컬럼은 xx/xx/xx로 저장되어 있습니다. xxxxxx형태로 변경해서 출력합니다.
-SELECT FIRST_NAME, LAST_NAME, REPLACE(HIRE_DATE,'/','') FROM EMPLOYEES ORDER BY FIRST_NAME;
+SELECT CONCAT(FIRST_NAME ||' ', LAST_NAME)AS 이름, 
+REPLACE(HIRE_DATE,'/','') AS 입사일자
+FROM EMPLOYEES ORDER BY 이름;
 
 --문제 2.
 --EMPLOYEES 테이블 에서 phone_numbe컬럼은 ###.###.####형태로 저장되어 있다
 --여기서 처음 세 자리 숫자 대신 서울 지역변호 (02)를 붙여 전화 번호를 출력하도록 쿼리를 작성하세요
 SELECT LPAD(SUBSTR(PHONE_NUMBER, 4),11,'02') FROM EMPLOYEES;
+-- CONCAT ( SUBSTR())도 가능
 
 --문제 3. EMPLOYEES 테이블에서 JOB_ID가 it_prog인 사원의 이름(first_name)과 급여(salary)를 출력하세요.
 --조건 1) 비교하기 위한 값은 소문자로 입력해야 합니다.(힌트 : lower 이용)
@@ -47,8 +50,9 @@ SELECT LPAD(SUBSTR(PHONE_NUMBER, 4),11,'02') FROM EMPLOYEES;
 --조건 3) 급여는 전체 10자리로 출력하되 나머지 자리는 *로 출력합니다. 
 --이 열의 열 별칭은 salary입니다.(힌트 : lpad 이용)
 
-SELECT * FROM EMPLOYEES;
-SELECT RPAD(SUBSTR(FIRST_NAME, 1,3),LENGTH(FIRST_NAME),'*')AS NAME, LPAD(SALARY , 10 , '*') FROM EMPLOYEES WHERE JOB_ID='IT_PROG';
+SELECT RPAD(SUBSTR(FIRST_NAME, 1,3),LENGTH(FIRST_NAME),'*')AS NAME,
+LPAD(SALARY , 10 , '*') 
+FROM EMPLOYEES WHERE LOWER(JOB_ID)='it_prog';
 
 
 
